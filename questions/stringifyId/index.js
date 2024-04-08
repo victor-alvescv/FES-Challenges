abc2cc88c76663c8f1f56b29c8e8c2d57aed68f6
Victor Alves <victoralvescabral7@gmail.com>
2024-04-08 18:46:57 -0300

    Second Commit

--- questions/stringifyId/index.js
+++ questions/stringifyId/index.js
@@ -17,7 +17,7 @@
  */
 
 const stringifyId = (arr) => {
-   const idArray = arr.map((obj) => obj.id)
+   const idArray = arr.map(obj => obj.id);
    const uniqueArray = [...new Set(idArray)]
    return uniqueArray.join(', ')
 };
