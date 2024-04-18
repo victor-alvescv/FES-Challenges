7d2bf1fb87344af2f397c4d1f79af47d1a2b46f9
Victor Alves <victoralvescabral7@gmail.com>
2024-04-17 21:13:17 -0300

    Pyramid

--- questions/pyramid/index.js
+++ questions/pyramid/index.js
@@ -1,5 +1,5 @@
 /** Print out steps for a pyramid based on a given positive integer N.
- * For each line, print out the character '#' to demonstrate a step 
+ * For each line, print out the character '#' to demonstrate a step
  * and make sure to use spaces to fill in the line.
  *
  * @example
@@ -18,7 +18,20 @@
  */
 
 const pyramid = (n) => {
-  
+  const columnWidth = n * 2 - 1;
+  const middle = Math.floor(columnWidth / 2);
+
+  for (let row = 0; row < n; row++) {
+    let step = "";
+    for (let column = 0; column < columnWidth; column++) {
+      if (column >= middle - row && column <= middle + row) {
+        step += "#";
+      } else {
+        step += " ";
+      }
+    }
+    console.log(step);
+  }
 };
 
 module.exports = pyramid;
--- solutions/pyramid/index.js
+++ solutions/pyramid/index.js
@@ -17,22 +17,26 @@
  *  '#######'
  */
 
+/* 
+BEST SOLUTION -> We create a columnWidth variable to calculate how big the width will be, using an algorithm! We calculate the middle of the column and create a for loop that loops over the 
+rows, since n represents the number of rows, this is not a square anymore! We create a step variable again, and other for loop for the columns, using the columnWidth that we calculated
+previously. Inside the for loop we create an if statement that say that if the column number in the iteration is bigger than the middle - the row number in the iteration and if the column
+is smaller than the middle plus the row number in the iteration, add a #, otherwise add a space. Add hashtags to the middle element - the row and the middle element + the row;
+*/
  const pyramid = (n) => {
-  const columnWidth = 2 * n - 1;
+  const columnWidth = n * 2 - 1;
   const middle = Math.floor(columnWidth / 2);
 
-  for (let row = 0; row < n; ++row) {
+  for (let row = 0; row < n; row++) {
     let step = "";
-
-    for (let column = 0; column < columnWidth; ++column) {
+    for (let column = 0; column < columnWidth; column++) {
       if (column >= middle - row && column <= middle + row) {
-        step += "#"
-      }
-      else {
-        step += " "
+        step += "#";
+      } else {
+        step += " ";
       }
     }
-    console.log(step)
+    console.log(step);
   }
 };
 
